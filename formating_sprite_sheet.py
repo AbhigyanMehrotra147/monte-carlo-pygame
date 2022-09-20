8f2b43ca4c065e7a3bc0916b1d779037f99e69ce
Chandan Yadav <97881807+chandan154@users.noreply.github.com>
2022-09-20 15:53:17 +0530

    + background and the json of the todler sprites

--- formating_sprite_sheet.py
+++ formating_sprite_sheet.py
@@ -1,6 +1,6 @@
 import json
 
-with open('boy_run.json') as file:
+with open('assets/boy-run/boy_run.json') as file:
     json_object = json.load(file)
     x = (json.dumps(json_object, indent = 3))
     boy_run_data = x
