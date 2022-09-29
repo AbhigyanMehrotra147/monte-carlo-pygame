e5a28e894ea4352c25438298ee0981cc6db9e9ce
Chandan Yadav <97881807+chandan154@users.noreply.github.com>
2022-09-29 09:34:06 +0530

    *

---
+++ BeerFormula.py
@@ -0,0 +1,41 @@
+# # line_coords = [(0.1,0.8),(0.9,0.8)]
+# # color_
+#
+#
+# import pygame
+#
+#
+# # this will have the right corner screen. where the formula will be blitted
+# pygame.init()
+# # class BeerFormuls:
+# #
+# #     def __int__(self, formula_screen_blit_surface: pygame.Surface, form_screen_cord:):
+# #         self._formula_screen_blit_surface = formula_screen_blit_surface
+# #         self._
+# #
+# #     # rectange will be created here
+# #     def _formula_screen_create(self,  ):
+# #         pygame.draw.rect(self._formula_screen_blit_surface, colour, )
+# #
+# #
+# #     def handle(self):
+# #         for event in pygame.event.get():
+# #             if( event.type == pygame.QUIT ):
+# #                 self._running = False
+# #             elif event.type == pygame.VIDEORESIZE:
+# #                 # in the running frame it is updated later on:
+# #                 print( 'Updated' )
+# #                 self._SIZE = event.dict["size"]
+# size = (1600, 1000)
+# flags = pygame.RESIZABLE
+# screen = pygame.display.set_mode(size, flags)
+# image = pygame.image.load()
+# surface = pygame.Surface()
+#
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#         # elif event.type == pygame.VIDEORESIZE:
+#         #     size = event.dict["size"]
+
