b29923dbdcdcc84a12bc77a78a21ba17f575d387
Vishal Paudel <95016059+VishalPaudel@users.noreply.github.com>
2022-09-30 22:04:16 +0530

    * changed the poisson file to have comments now

--- Poisson.py
+++ Poisson.py
@@ -7,10 +7,10 @@ import copy
 class Poison():
 
     def __init__( self, t: int = 3, Lambda: int = 5, N: int = 10 ** 5, beer_price: int = 350):
-        self._t = t
-        self._Lambda = Lambda
-        self._N = N
-        self._beer_price = beer_price
+        self._t = t                                                                                 # the number of hours he works in one day
+        self._Lambda = Lambda                                                                       # The expected number of people in one hour
+        self._N = N                                                                                 # The upper limit on the cur_index
+        self._beer_price = beer_price                                                               # the price of the beer
 
         # the current running index / day
         self._cur_index = 0
@@ -19,7 +19,7 @@ class Poison():
         self._beer_list = np.array( [], dtype=int )
 
     def update( self ):
-        # a new day, a fresh calculation
+        # a new day, a fresh calculation to store in self._beer_list
         n = np.random.poisson( self._Lambda * self._t )                                             # using numpy to generate random poisson val
 
         coins = []                                                                                  # initialising with empty array
@@ -32,15 +32,20 @@ class Poison():
 
                 U = random.uniform(0, 1)                                                            # random values between 0 & 1
 
-                coins[ j, 0 ] = ( U <= 2 / 5 ) * 5 + ( 2 / 5 < U <= 4 / 5 ) * 10 + ( U > 4 / 5 ) * 20
+                coins[ j, 0 ] = ( U <= 2 / 5 ) * 5 + ( 2 / 5 < U <= 4 / 5 ) * 10 + ( U > 4 / 5 ) * 20   # short hand condition
                 # updating the (j, 0) entry of the array
+
         # print( self._cur_index )
         # appending the new beer got or not got inside the beer list
         self._beer_list = np.append( self._beer_list, int( np.sum( coins ) >= self._beer_price ) )  # the sum of coins vs the price, happy sad
 
+        # updating the cur index to next day
         self._cur_index += 1
 
     def get_list( self, from_index: int = 0, to_index: int = None ):
+        """
+        returns the self._beer_list
+        """
         if( to_index is None ):
             to_index = self._cur_index
 
@@ -50,13 +55,18 @@ class Poison():
         return copy.deepcopy( self._beer_list[ from_index : to_index + 1 ] )
 
     def get_cur_index( self ):
+        """
+        returns the current running index
+        """
         return copy.deepcopy( self._cur_index )
 
     def get_mean_array( self ):
         return self._beer_list.mean()
 
 def main():
-
+    """
+    go to the lower most if statement in global scope, if this seems out of place
+    """
     t = 3
     Lambda = 5
     N = 10 ** 6
@@ -66,6 +76,7 @@ def main():
 
     till = 10**3
 
+    # run till 10**3 entries, rendering self._N useless in this case
     while( pois.get_cur_index() < till ):
         pois.update()
         # print( pois.get_list() )
@@ -74,5 +85,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
