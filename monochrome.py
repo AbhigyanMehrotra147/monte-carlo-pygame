344ac6e77dc80cd88417783eb4ddc62f49613825
Vishal Paudel <95016059+VishalPaudel@users.noreply.github.com>
2022-09-30 14:02:47 +0530

    + ?

---
+++ monochrome.py
@@ -0,0 +1,17 @@
+
+from PIL import Image
+
+def make_mono( file: str = 'zi.png', thresh = 200, reverse: bool = False ):
+    # image_file = Image.open( file ) # open colour image
+    # image_file = image_file.convert('1') # convert image to black and white
+    # image_file.save( 'zi.png' )
+
+    # white if greater than threshold
+    image_file = Image.open( file )
+    filter = lambda x : 255 * int( not( reverse  ) ) if x > thresh else 0
+    image_file = image_file.convert( 'L' ).point( filter, mode='1' )
+    image_file.save( file )
+    print( 'Saved the converted image' )
+
+if( __name__ == "__main__" ):
+    make_mono()
